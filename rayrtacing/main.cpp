df15d18db24e9b0e18f3a57a6f162eec971f37bb
hawawaii <fnzkaps@naver.com>
2025-10-01 13:31:27 +0900

    초기 커밋.

---
+++ rayrtacing/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//이미지
+
+	int image_width = 256;
+	int image_height = 256;
+
+	//렌더러
+
+	cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+}
