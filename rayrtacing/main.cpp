39a08131e8ec0dbe4f244d8aee2092b2dafc5247
hawawaii <fnzkaps@naver.com>
2025-10-17 01:24:36 +0900

    vec3.h added

--- rayrtacing/main.cpp
+++ rayrtacing/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 using namespace std;
 
+#include "vec3.h"
+
 int main() {
 
-	//이미지
+	//image
 
 	int image_width = 256;
 	int image_height = 256;
 
-	//렌더러
+	//renderer
 
 	cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+		clog << "\rScanlines remaining: " << image_height - j - 1 << ' ' << flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -22,8 +25,8 @@ int main() {
             int ig = int(255.999 * g);
             int ib = int(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
-
+    clog << "\rDone.                 \n";
 }
