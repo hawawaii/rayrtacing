895a7a09d26505f7444421012e1e1e34e4d8024a
hawawaii <fnzkaps@naver.com>
2025-10-17 01:46:56 +0900

    added color.h ch3.1

--- rayrtacing/main.cpp
+++ rayrtacing/main.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 using namespace std;
 
+#include "color.h"
 #include "vec3.h"
 
 int main() {
 
-	//image
+    // Image
 
-	int image_width = 256;
-	int image_height = 256;
+    int image_width = 256;
+    int image_height = 256;
 
-	//renderer
+    // Render
 
-	cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
-		clog << "\rScanlines remaining: " << image_height - j - 1 << ' ' << flush;
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
-    clog << "\rDone.                 \n";
+
+    std::clog << "\rDone.                 \n";
 }
